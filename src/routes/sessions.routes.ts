40bc9834d1f5d83777404f175b94f12a4bb919d4
Debora Souza <debora_rss@hotmail.com>
2020-07-02 14:25:11 -0300

    Generate jwt token

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,14 +10,17 @@ sessionsRouter.post('/', async(request, response) => {
 
     const authenticateUser = new AuthenticateUserService();
 
-    const { user } = await authenticateUser.execute({ 
+    const { user, token } = await authenticateUser.execute({ 
       email, 
       password 
     });
 
     delete user.password;
 
-    return response.status(200).json({ user });
+    return response.status(200).json({ 
+      user,
+      token 
+    });
   } catch(err) {
     return response.status(400).json({ error: err.message });
   }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User'
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -29,9 +31,15 @@ class AuthenticateUserService {
     if(!passwordMatched) {
       throw new Error('Incorrect email/password combination');
     }
+
+    const token = sign({}, '', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
     
     return {
       user,
+      token
     }
   }
 }
